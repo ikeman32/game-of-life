0a547e17feaf50ef4a12f3a41e27b009a7a680b4
David Isakson <ikeman32@yahoo.com>
2020-06-22 17:49:34 -0700

    basic layout complete

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ function App() {
       <header><h3 id='title'>Game of Life</h3></header>
       <div className='container'>
         <div className='game'></div>
-        <div className='rules'></div>
+        <div className='rules'>
+          <h2>Rules</h2>
+          <p>
+            <ul>
+              <li>
+                Rule 1
+              </li>
+              <li>Rule 2</li>
+            </ul>
+          </p>
+        </div>
         <div className='about'>
+          <h2>About this Algorithm</h2>
           <p>Esse officia ipsum sint voluptate qui est Lorem eu. Dolore nisi commodo esse nostrud cupidatat ut magna non labore eu magna exercitation nisi. Incididunt proident tempor ea esse veniam fugiat quis ex adipisicing duis ea ullamco nulla tempor. Quis ut non labore eu aute. Eiusmod enim laborum culpa ipsum cillum minim in nostrud enim. Velit Lorem cillum excepteur eiusmod quis id magna irure excepteur magna duis esse proident.</p>
         </div>
       </div>
