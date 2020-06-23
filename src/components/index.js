d279928b38ab3bf4349e9a8c140b49d45c94c51b
David Isakson <ikeman32@yahoo.com>
2020-06-23 10:58:23 -0700

    Begin setup of game board

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+//Import components to here
+import Square from './square';
+
+/*
+Then export from here so that imports to other sections take
+the form:
+import {<Component Name>, <Component Name>, ...} from '<path to components folder>';
+*/
+export{
+    Square
+}
---
+++ src/components/square/square.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './square.scss'
+class Square extends Component{
+    render(){
+        <button className='button'>
+            {/* TODO */}
+        </button>
+    }
+}
+
+export default Square;
