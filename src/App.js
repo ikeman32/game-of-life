b3f3343d7168e7839bfcec3fda0339843dde45e0
David Isakson <ikeman32@yahoo.com>
2020-06-23 10:57:59 -0700

    Basic cosmetic setup complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header><h3 id='title'>Game of Life</h3></header>
+      <header><h3 id='title'>Conway's Game of Life</h3></header>
       <div className='container'>
         <div className='game'></div>
         <div className='rules'>
@@ -12,18 +12,31 @@ function App() {
           <p>
             <ul>
               <li>
-                Rule 1
+                Rule 1: Any cell with two or three neighbors survives.
               </li>
-              <li>Rule 2</li>
+              <li>
+                Rule 2: Any dead cell with two or three live cells is resurected.
+              </li>
+              <li>
+                Rule 3: All other live cells die in the next generation and any remaining dead cells remain dead.
+              </li>
             </ul>
           </p>
         </div>
         <div className='about'>
           <h2>About this Algorithm</h2>
-          <p>Esse officia ipsum sint voluptate qui est Lorem eu. Dolore nisi commodo esse nostrud cupidatat ut magna non labore eu magna exercitation nisi. Incididunt proident tempor ea esse veniam fugiat quis ex adipisicing duis ea ullamco nulla tempor. Quis ut non labore eu aute. Eiusmod enim laborum culpa ipsum cillum minim in nostrud enim. Velit Lorem cillum excepteur eiusmod quis id magna irure excepteur magna duis esse proident.</p>
+          <p>
+          <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">Conway’s Game of Life</a> was devised by British mathematician John Horton Conway and is a demonstration of cellular automaton. Cellular automaton is a discrete model studied in automata theory, which is the study of abstract machines and automata, as well as the computational problems that can be solved by them. His primary motivations came from questions in mathematical logic and by work on simulation games by Ulam (<a href="https://en.wikipedia.org/wiki/Stanislaw_Ulam">Stainslaw Marcin Ulam</a>, a Polish scientist in the field of mathematics and nuclear physics) and others. Conway started doing experiments in 1968 with a variety of two-dimensional cellular automaton rules.
+          </p>
+          <p>
+          The first appearance of the game came in October 1970 issue of Scientific American, in Martin Gardner’s “Mathematical Games” column. In the column he wrote, “Because of Life’s analogies with the rise, fall and alterations of a society of living organisms, it belongs to a growing class of what are called ‘simulation games’ (games that resemble real life processes).
+          </p>
+          <p>
+          The game is considered <a href="https://en.wikipedia.org/wiki/Turing_completeness">Turing Complete</a> and can simulate a <a href="https://en.wikipedia.org/wiki/Von_Neumann_universal_constructor">Universal Constructor</a> or any other <a href="https://en.wikipedia.org/wiki/Turing_machine">Turing Machine</a>. A Turing Machine is an abstract mathematical computational device named after <a href="https://en.wikipedia.org/wiki/Alan_Turing">Alan Turing</a>, who developed the <a href="https://en.wikipedia.org/wiki/Turing_test">Turing Test</a>, at test to determine if a machine exhibits intelligent behavior.
+          </p>
         </div>
       </div>
-      
+      <footer><p>By David Isakson</p></footer>
     </div>
   );
 }
