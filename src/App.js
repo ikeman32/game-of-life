2aff46844b5549418177cd15e15beb3f2ddad8e2
David Isakson <ikeman32@yahoo.com>
2020-06-22 17:35:37 -0700

    setting layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Conway's Game of Life</h3>
-      </header>
+      <header><h3 id='title'>Game of Life</h3></header>
+      <div className='container'>
+        <div className='game'></div>
+        <div className='rules'></div>
+        <div className='about'>
+          <p>Esse officia ipsum sint voluptate qui est Lorem eu. Dolore nisi commodo esse nostrud cupidatat ut magna non labore eu magna exercitation nisi. Incididunt proident tempor ea esse veniam fugiat quis ex adipisicing duis ea ullamco nulla tempor. Quis ut non labore eu aute. Eiusmod enim laborum culpa ipsum cillum minim in nostrud enim. Velit Lorem cillum excepteur eiusmod quis id magna irure excepteur magna duis esse proident.</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
